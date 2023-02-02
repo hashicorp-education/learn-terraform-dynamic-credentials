0f30af269ee8736eaac27f37b62c70e4f36dcc49
Robin Norwood <rnorwood@hashicorp.com>
2023-02-02 11:40:08 -0600

    Update config w/namespace

--- vault/infra/main.tf
+++ vault/infra/main.tf
@@ -2,12 +2,9 @@ provider "vault" {
   address = var.vault_url
 }
 
-# resource "vault_namespace" "test" {
-#   path = "test"
-# }
-
 resource "vault_mount" "example" {
-  # namespace = vault_namespace.test.path_fq
+  namespace = "admin"
+
   path    = "example"
   type    = "kv"
   options = { version = "2" }
@@ -15,7 +12,6 @@ resource "vault_mount" "example" {
 
 resource "vault_kv_secret_v2" "example" {
   mount               = vault_mount.example.path
-  # namespace = vault_namespace.test.path_fq
 
   name                = "unsecret"
   cas                 = 1
